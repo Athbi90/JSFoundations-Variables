b54aa452af73025d1f923de0be9aa1f72df7d2f1
Athbi90 <Athbi90@gmail.com>
2021-02-01 19:27:08 +0300

    add another experience variable

--- story.js
+++ story.js
@@ -1,5 +1,11 @@
 let myself = "Athbi";
 let age = 30;
 let hobby = "Shooting";
+let exp = 15;
+
 
 console.log ("Mr." + myself + ", in 33 years, you will be "+ (age+33) + " years old. By then you'll have decades of " + hobby + " practice!");
+
+
+console.log ("Mr." + myself + ", in " + exp + "years, you will be "+ (age+exp) + " years old. By then you'll have decades of " + hobby + " practice!");
+
