03640dd07c2859fdf95b047a619a87c965ad6afd
Athbi90 <Athbi90@gmail.com>
2021-02-01 18:47:29 +0300

    Task #3 has been completed!

---
+++ story.js
@@ -0,0 +1,5 @@
+let myself = "Athbi";
+let age = 30;
+let hobby = "Shooting";
+
+console.log ("Mr." + myself + ", in 33 years, you will be "+ (age+33) + " years old. By then you'll have decades of " + hobby + " practice!");
